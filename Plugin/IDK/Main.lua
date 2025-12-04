aeceb0aa86ce8270e1113b296fe196de7904dddb
thanhtv68 <thanhaccxbox12@gmail.com>
2025-12-04 16:51:08 +0700

    Update Main.lua

--- Plugin/IDK/Main.lua
+++ Plugin/IDK/Main.lua
@@ -1,1 +1,17 @@
+["Addon Pack"] = {
+    url = "https://raw.githubusercontent.com/s-0-a-d/Overdive-H/refs/heads/main/Plugin/Addon%20Pack",
+    credits = "lzzzx",
+    date = "1/12/2025"
+}
 
+["Auto Bomb Jump"] = {
+    url = "https://raw.githubusercontent.com/s-0-a-d/Overdive-H/refs/heads/main/Plugin/Auto%20Bomb%20Jump",
+    credits = "kenzie",
+    date = "29/11/2025"
+}
+
+["Throw Knife Esp"] = {
+    url = "https://raw.githubusercontent.com/s-0-a-d/Overdive-H/refs/heads/main/Plugin/Throwing%20Knife%20Esp",
+    credits = "Mồn Lèo",
+    date = "30/11/2025"
+}
